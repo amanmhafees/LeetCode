37fec2583e90035e805396fda0c7b62ddcdf6c17
Aman M Hafees <amanmhafeeswork@gmail.com>
2025-07-24 22:08:19 +0530

    Time: 8 ms (97.37%) | Memory: 12.3 MB (77.19%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def reverse(self, x):
         res = 0
-        limit_low, limit_high = -2**31, 2**31 - 1
-        n = abs(x)
-        sign = -1 if x < 0 else 1
+        n, sign = abs(x), 1 if x >= 0 else -1
 
         while n:
             res = res * 10 + n % 10
             n //= 10
 
         res *= sign
-        return res if limit_low <= res <= limit_high else 0
+
+        return res if -2**31 <= res <= 2**31 - 1 else 0
