55121d9748b9b91498af5966b7416a7ee78d93c5
Aman M Hafees <amanmhafeeswork@gmail.com>
2025-07-24 20:29:18 +0530

    Time: 7 ms (64.05%) | Memory: 12.6 MB (15.75%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        for i in range(9):
+            row_check=["1","2","3","4","5","6","7","8","9"]
+            col_check=["1","2","3","4","5","6","7","8","9"]
+            for j in range(9):
+                if board[i][j]!=".":    
+                    if board[i][j] in row_check:
+                        row_check.remove(board[i][j])
+                    else:
+                        return False
+                if board[j][i]!=".":
+                    if board[j][i] in col_check:
+                        col_check.remove(board[j][i])
+                    else:
+                        return False
+        for curr_row in range(0,9,3):
+            for curr_col in range(0,9,3):
+                check=["1","2","3","4","5","6","7","8","9"]
+                for i in range(3):
+                    for j in range(3):
+                        val = board[curr_row + i][curr_col + j]
+                        if val != ".":
+                            if val in check:
+                                    check.remove(val)
+                            else:
+                                return False
+
+        return True
+                    
+                    
+            
+        
