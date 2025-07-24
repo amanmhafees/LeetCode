7f2c8cf5f56c982b08ce43b5eea2e23e11bd0a5e
Aman M Hafees <amanmhafeeswork@gmail.com>
2025-07-24 22:03:04 +0530

    Time: 24 ms (17.33%) | Memory: 12.5 MB (49.55%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        res=0
+        n=abs(x)
+        while n>0:
+            res=res*10+n%10
+            n//=10
+        if x<0:
+            res=-res
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+
+        return res
+        
