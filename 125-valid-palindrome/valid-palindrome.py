0f672b0c3d7ec6e76ce2572a9ae7f3c9f1be8567
Aman M Hafees <amanmhafeeswork@gmail.com>
2025-08-02 09:35:49 +0530

    Time: 140 ms (18.44%) | Memory: 13.7 MB (41.04%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    
+    def isPalindrome(self, s):
+        com_string=self.extract_alphabet(s)
+        length_s=len(com_string)
+        if(length_s==0):
+            return True
+        for i in range(length_s/2):
+            if com_string[i] !=com_string[length_s-i-1]:
+                return False
+        return True
+    def extract_alphabet(self,s):
+        new_string=""
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                new_string+=i.lower()
+        return new_string
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
