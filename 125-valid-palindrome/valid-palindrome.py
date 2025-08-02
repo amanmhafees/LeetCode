ee42ec65e76affdc52f4dca075b6d9e71185279e
Aman M Hafees <amanmhafeeswork@gmail.com>
2025-08-02 09:38:26 +0530

    Time: 159 ms (9.35%) | Memory: 13 MB (68.77%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,10 +5,11 @@ class Solution(object):
         length_s=len(com_string)
         if(length_s==0):
             return True
-        for i in range(length_s/2):
-            if com_string[i] !=com_string[length_s-i-1]:
-                return False
-        return True
+        # for i in range(length_s/2):
+        #     if com_string[i] !=com_string[length_s-i-1]:
+        #         return False
+        # return True
+        return com_string==com_string[::-1]
     def extract_alphabet(self,s):
         new_string=""
         for i in s:
